907a8cd0165ba7c3be980abdef24cf43335818b4
Vladyslav <cooldude@list.ru>
2016-03-25 11:55:57 +0200

    list of modalities

--- sampleTest.py
+++ sampleTest.py
@@ -18,4 +18,34 @@ for row in cursor:
 
 con.close()
 print("Connection is closed")
+"""
+Ultrasound Multi-frame Image Storage
+1.2.840.10008.5.1.4.1.1.3.1
+Ultrasound Multi-Frame Image Storage (Retired)
+1.2.840.10008.5.1.4.1.1.3
+Ultrasound Image Storage
+1.2.840.10008.5.1.4.1.1.6.1
+Ultrasound Image Storage (Retired)
+1.2.840.10008.5.1.4.1.1.6
+Secondary Capture Image Storage
+1.2.840.10008.5.1.4.1.1.7
+X-Ray Angiographic Image Storage
+1.2.840.10008.5.1.4.1.1.12.1
+X-Ray Radiofluoroscopic Image Storage
+1.2.840.10008.5.1.4.1.1.12.2
+Basic Text SR Storage
+1.2.840.10008.5.1.4.1.1.88.11
+Enhanced SR Storage
+1.2.840.10008.5.1.4.1.1.88.22
+Comprehensive Structured Reporting SOP Class
+1.2.840.10008.5.1.4.1.1.88.33
+Positron Emission Tomography Image Storage
+1.2.840.10008.5.1.4.1.1.128
+Computed Radiography Image Storage
+1.2.840.10008.5.1.4.1.1.1
+CT Image Storage
+1.2.840.10008.5.1.4.1.1.2
+Study Root Query/Retrieve Information Model – MOVE
+1.2.840.10008.5.1.4.1.2.2.2
+"""
 # modified on home PC
